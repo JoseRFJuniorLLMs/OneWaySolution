fa41b1a0c5dfd042eee9a0f395c35ab744c84dd8
Matheus Willian <matheuswilliandf@gmail.com>
2018-07-16 19:51:55 -0300

    security

--- crawler.py
+++ crawler.py
@@ -1,11 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+Este módulo contém funções para:
++ [x] Consultar .
++ [x] Obter palavras-chave.
+'''
+
 from requests import get
 from json import dumps
-url = 'https://demoowshq.search.windows.net/indexes/teste/docs?api-version=2017-11-11'
-headers = {"api-key":'FB2F6A27BCEB99C468EF84D9A682E45B'} 
-params = {"$select":'metadata_storage_name,keyphrases'}
 
+#%%
+url = 'https://demoowshq.search.windows.net/indexes/teste/docs?api-version=2017-11-11'
+headers = {"api-key":'******************************'} 
+params = {"$select":'metadata_storage_name,keyphrases'}
 response = get(url, headers = headers, params = params) 
-# print(response.url)
+print(len(response.url))
+
 results = response.json()
 
 with open('api-text.txt','w') as fl:
