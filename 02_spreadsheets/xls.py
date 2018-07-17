c0843828b2698bec21c5beac2a86cbf1551a35db
Matheus Willian <matheuswilliandf@gmail.com>
2018-07-17 00:06:28 -0300

    xls changes

--- 02_spreadsheets/xls.py
+++ 02_spreadsheets/xls.py
@@ -1,22 +1,18 @@
-# -*- coding: utf-8 -*-
-'''
-Este módulo contém funções para:
-- [x] Transformar planilha em dataset.
-- [ ] Adicionar palavras chaves.
-'''
 #%%
-import pandas as pd
+import os
+os.chdir('./02_spreadsheets')
 
 #%%
+import pandas as pd
 xls2 = pd.read_excel('caixa.xls', sheet_name = 'REL. DOCUMENTOS COPIADOS')
 xls3 = pd.read_excel('caixa.xls', sheet_name = 'COLUNAS A APRESENTAR')
 xls3.head()
 
 #%%
-xls = pd.merge(xls3, xls2)
+xls = pd.merge(xls2, xls3)
 xls.drop_duplicates(subset = 'Expediente', keep = False, inplace = True)
-xls.set_index(['Expediente', 'Nome do Arquivo'], inplace = True)
 xls.head()
+len(xls)
 
 #%%
 xls.to_csv('xls-dataset.csv', sep = ';', index = True)
